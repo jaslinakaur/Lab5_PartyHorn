c9e6509bcdee7c2bb5c3fa3bcd5fbb7237fe4f31
Jaslina Kochhar <jaslina.kaur@gmail.com>
2021-02-05 13:48:22 -0800

    Added some vars for different IDs from html doc

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,5 +3,13 @@
 // TODO
 // Input field that serves as the textual indicator of sound level
 
-var aud = document.getElementById("horn-sound");
-aud.volume = 0.2;
+var audtype = document.getElementById("horn-sound");
+var vol_slider = documnet.getElementById("volume-slider");
+var horn_image = documnet.getElementById("sound-image");
+var vol_image = documnet.getElementById("volume-image");
+var vol_num = documnet.getElementById("volume-number");
+var radio_btns = documnet.getElementById("audio-selection");
+
+document.getElementById("honk-btn").addEventListener("click", function() {
+    alert("Hello World!");
+  });
